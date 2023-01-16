4b3d9bbbe9a47b16ab35ee4a7a299f5dce305740
AleksArden <i1723@i.ua>
2023-01-16 16:34:08 +0200

    add start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,15 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <nav>
+        <Link to="/movie">Movie</Link>
+      </nav>
+      <Routes>
+        <Route path="/movie" element={<div>Movie</div>} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </div>
+  );
 };
